4271ff7d9ce11f897fa78eb26d0293a81c16faff
danielvialcruz-del <danielvialcruz.del@gmail.com>
2025-11-24 00:07:44 -0300

    Exportar todos los módulos del menú

--- src/paginas/index.ts
+++ src/paginas/index.ts
@@ -1,1 +1,17 @@
-export { default as Reportes } from './reportes'; 
+import Inicio from "./Inicio";
+import Clientes from "./Clientes";
+import Proyectos from "./Proyectos";
+import Proveedores from "./Proveedores";
+import Cotizaciones from "./Cotizaciones";
+import Presupuestos from "./Presupuestos";
+import NotasVenta from "./NotasVenta";
+
+export {
+  Inicio,
+  Clientes,
+  Proyectos,
+  Proveedores,
+  Cotizaciones,
+  Presupuestos,
+  NotasVenta
+};
